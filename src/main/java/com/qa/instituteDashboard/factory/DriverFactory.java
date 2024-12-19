7bd43e509bff7c6948422702a26b3f81add39ec6
nikhil <nikhil.kumar@leo1.in>
2024-12-19 17:00:16 +0530

    added extent & allure reports in code

--- src/main/java/com/qa/instituteDashboard/factory/DriverFactory.java
+++ src/main/java/com/qa/instituteDashboard/factory/DriverFactory.java
@@ -3,22 +3,30 @@ package com.qa.instituteDashboard.factory;
 import com.qa.instituteDashboard.errors.AppError;
 import com.qa.instituteDashboard.exceptions.BrowserException;
 import com.qa.instituteDashboard.exceptions.FrameworkException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+
+import static java.sql.DriverManager.getDriver;
 
 public class DriverFactory {
     WebDriver driver;
     Properties prop;
 
     public static String isHighlight;
+
+    public static ThreadLocal<WebDriver> tldriver = new ThreadLocal<WebDriver>();
     /**
      * This method is to init the driver on basis of given browser name
 //     * @param browserName
@@ -31,28 +39,43 @@ public class DriverFactory {
         System.out.println("browser name is : "+ browserName);
 
         isHighlight = prop.getProperty("highlight");
+
+        OptionsManager optionsManager = new OptionsManager(prop);
+
         switch (browserName) {
             case "chrome":
-                driver = new ChromeDriver();
+//                driver = new ChromeDriver();
+                tldriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
                 break;
             case "firefox":
-                driver = new FirefoxDriver();
+//                driver = new FirefoxDriver();
+                tldriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
                 break;
             case "edge":
-                driver = new EdgeDriver();
+//                driver = new EdgeDriver();
+                tldriver.set(new EdgeDriver(optionsManager.getEdgeOptions()));
                 break;
             case "safari":
-                driver = new SafariDriver();
+//                driver = new SafariDriver();
+                tldriver.set(new SafariDriver());
                 break;
+
             default:
                 System.out.println(AppError.INVALID_BROWSER_MESG + browserName + " Is InValid" );
                 throw new BrowserException(AppError.INVALID_BROWSER_MESG+ browserName);
         }
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.get(prop.getProperty("url"));
-        return driver;
+        getDriver().manage().window().maximize();
+        getDriver().manage().deleteAllCookies();
+        getDriver().get(prop.getProperty("url"));
+        return getDriver();
 }
+
+
+public static  WebDriver getDriver(){
+        return tldriver.get();
+}
+
+
 // to run tests in maven, install mvn
 //    mvn clean install -Denv ="qa"
 
@@ -92,4 +115,19 @@ public Properties initProp(){
         }
         return prop;
 }
+    /**
+     * take screenshot
+     */
+    public static String getScreenshot(String methodName) {
+        File srcFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE); // temp dir
+        String path = System.getProperty("user.dir") + "/screenshot/" + methodName + "_" + System.currentTimeMillis() + ".png";
+        File destination = new File(path);
+        try {
+            FileHandler.copy(srcFile, destination);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return path;
+    }
+
 }
