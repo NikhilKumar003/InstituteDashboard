2235b8d54002fabe1af83a6412a744582fb85208
nikhil <nikhil.kumar@leo1.in>
2024-12-26 16:22:00 +0530

    added few methods in leo1 card page

--- src/main/java/com/qa/instituteDashboard/pages/Leo1CardPage.java
+++ src/main/java/com/qa/instituteDashboard/pages/Leo1CardPage.java
@@ -15,28 +15,57 @@ public class Leo1CardPage {
     private By downloadTemplateBtn = By.xpath("//button[contains(text(),'Download Template')]");
     private By uploadExcelBtn = By.xpath("//span[contains(text(),'Upload excel')]");
     private By downloadTableBtn = By.xpath("//button[contains(text(),'Download table')]");
+    private By resetBtn = By.xpath("//span[contains(text(),'Reset')]");
+    private By showBtn = By.cssSelector("#MainScrollbar > div:nth-child(4) > div >div >a >u");
+    private By submitBtn = By.xpath("//button[contains(text(),'Submit')]");
+    private By clearBtn = By.xpath("//button[contains(text(),'Clear')]");
+    private By mobileNoText = By.xpath("//input[@placeholder='Mobile No.']");
+    private By deliverStudentBtn = By.xpath("//button[contains(text(),'Delivered to student:')]");
 
 
     public Leo1CardPage(WebDriver driver) {
-        this.driver=driver;
+        this.driver = driver;
         eleUtil = new ElementUtil(driver);
     }
 
-    public String getPageUrl(){
-        String url= eleUtil.waitforURLContainsAndReturns(AppConstants.LEO1_CARD_PAGE_FRACTION_URL,AppConstants.DEFAULT_SHORT_TIME_OUT);
-        System.out.println("Leo1 Card page url :"+ url);
+    public String getPageUrl() {
+        String url = eleUtil.waitforURLContainsAndReturns(AppConstants.LEO1_CARD_PAGE_FRACTION_URL, AppConstants.DEFAULT_SHORT_TIME_OUT);
+        System.out.println("Leo1 Card page url :" + url);
         return url;
     }
-    public boolean isDownloadTemplateBtn(){
-     return eleUtil.isElementDisplayed(downloadTemplateBtn);
+
+    public boolean isDownloadTemplateBtn() {
+        return eleUtil.isElementDisplayed(downloadTemplateBtn);
     }
 
-    public boolean isUploadExcelBtn(){
+    public boolean isUploadExcelBtn() {
         return eleUtil.isElementDisplayed(uploadExcelBtn);
     }
 
-    public boolean isdownloadTableBtn(){
-       return eleUtil.isElementDisplayed(downloadTableBtn);
+    public boolean isdownloadTableBtn() {
+        return eleUtil.isElementDisplayed(downloadTableBtn);
+    }
+    public boolean isResetBtn() {
+        return eleUtil.isElementDisplayed(resetBtn);
+    }
+    public boolean isSubmitAndClearBtn() {
+        eleUtil.waitforElementVisible(showBtn, AppConstants.DEFAULT_SHORT_TIME_OUT).click();
+        eleUtil.scrollToElement(submitBtn);
+        return eleUtil.isElementDisplayed(submitBtn);
+    }
+    public void verifyAllUsersLoaded(){
+        eleUtil.waitforElementVisible(showBtn, AppConstants.DEFAULT_SHORT_TIME_OUT).click();
+        eleUtil.scrollToElement(submitBtn);
+        eleUtil.doClick(showBtn);
+
+
     }
 
+    public void checkUserWithMobile(){
+        eleUtil.waitforElementVisible(showBtn, AppConstants.DEFAULT_SHORT_TIME_OUT).click();
+        eleUtil.scrollToElement(submitBtn);
+        eleUtil.waitforElementVisible(mobileNoText,AppConstants.DEFAULT_SHORT_TIME_OUT).sendKeys("7842290549");
+        eleUtil.waitforElementVisible(submitBtn,AppConstants.DEFAULT_MEDIUM_TIME_OUT).click();
+//        eleUtil.scrollToElement(deliverStudentBtn);
+    }
 }
