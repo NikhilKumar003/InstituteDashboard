90df72f3dc984d5aa5bf1efd3feec8e2f04c4f75
nikhil <nikhil.kumar@leo1.in>
2025-01-15 21:50:27 +0530

    added few test cases in fee structure

--- src/main/java/com/qa/instituteDashboard/pages/FeeStructurePage.java
+++ src/main/java/com/qa/instituteDashboard/pages/FeeStructurePage.java
@@ -17,6 +17,15 @@ public class FeeStructurePage  {
     private By branchDD = By.xpath("(//div[@class=' css-19bb58m'])[2]");
     private By resetBtn = By.xpath("//span[text()='Reset']");
     private By downloadBtn = By.xpath("//span[text()='Download All']");
+    private By addCourseHeader = By.xpath("//span[text()='Add New Course']");
+    private By courseDropDown = By.cssSelector("div.col-md-7 div.css-yi2ntm-control");
+    private By classDropDown = By.cssSelector("#react-select-11-placeholder");
+    private By enterCourseText = By.xpath("//input[@placeholder='Enter Course Name']");
+    private By enterAliasCourseText = By.xpath("//input[@placeholder='Enter Alias Course Name']");
+    private By classNoText = By.xpath("//input[@type='number']");
+    private By nextBtn = By.xpath("(//img[@decoding='async'])[10]");
+    private By addCourseBtn =By.xpath("//button[text()='Add New Course']");
+
 
 
     public FeeStructurePage(WebDriver driver) {
@@ -53,6 +62,17 @@ public class FeeStructurePage  {
     }
     public boolean isResetBtnExist(){
         return  eleUtil.isElementDisplayed(resetBtn);
+    }
+    public boolean addCourseBtn(){
+        eleUtil.waitforElementpresence(courseBtn,AppConstants.DEFAULT_MEDIUM_TIME_OUT).click();
+        eleUtil.waitforElementVisible(addCourseHeader,AppConstants.DEFAULT_SHORT_TIME_OUT);
+        eleUtil.selectDropDownText(classDropDown,"MUMBAI");
+        eleUtil.waitforElementVisible(enterCourseText,AppConstants.DEFAULT_SHORT_TIME_OUT).sendKeys("test-1");
+        eleUtil.waitforElementpresence(enterAliasCourseText,AppConstants.DEFAULT_SHORT_TIME_OUT).sendKeys("test");
+        eleUtil.waitforElementpresence(classNoText,AppConstants.DEFAULT_SHORT_TIME_OUT).sendKeys("2");
+        eleUtil.waitforElementpresence(nextBtn,AppConstants.DEFAULT_LONG_TIME_OUT).click();
+        eleUtil.waitforElementpresence(addCourseBtn,AppConstants.DEFAULT_MEDIUM_TIME_OUT).click();
+       return  eleUtil.isElementDisplayed(addCourseBtn);
     }
 
 }
